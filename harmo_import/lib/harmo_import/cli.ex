64b7728ce7acaf1862d53686d87a3cba47630620
Krzysztof Białek <krzysztof.bialek@gmail.com>
2020-03-23 22:46:24 +0100

    get access token

--- harmo_import/lib/harmo_import/cli.ex
+++ harmo_import/lib/harmo_import/cli.ex
@@ -6,30 +6,34 @@ defmodule HarmoImport.CLI do
   end
 
   def parse_args(args) do
-    parse = OptionParser.parse(args, switches: [ file: :string ],
+    parse = OptionParser.parse(args, switches: [ file: :string, password: :string, email: :string ],
                                      aliases:  [ f:    :file    ])
   
     case parse do
-      { [file: file], _, _ } 
-        -> file
+      { [file: file, email: email, password: password], _, _ } 
+        -> file, email, password
       _ ->
         IO.puts """
-          file must be present
+          file, email and password must be present
         """
 
         System.halt(0)
     end
   end
 
-  def process(file)) do
-    login()
-    |> importLog(file)
+  def process(file, email, password) do
+    login(email, password)
+    |> import_log(file)
   end
 
-  defp login() do
+  defp login(email, password) do
+    {:ok, body} = Poison.encode(%{data: %{ attributes: %{ email: email, password: password} } })
+    {:ok, response} = HTTPoison.post(url, body, [{"Content-Type", "application/vnd.api+json"}])
+    {:ok, data} = Poison.decode(response.body)
+    token = get_in(data, ["data", "attributes", "access-token"])
+    token
   end
 
-  defp importLog(file) do
+  defp import_log(user_token, file) do
   end
-    
 end
